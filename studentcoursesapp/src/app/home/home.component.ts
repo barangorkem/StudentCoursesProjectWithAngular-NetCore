bc1de0355d98a14f86b4d0cfcd4ae18a47b28a36
Baran <baran34yildiz@gmail.com>
2019-09-08 17:41:25 +0300

    Student class created and making validation

--- studentcoursesapp/src/app/home/home.component.ts
+++ studentcoursesapp/src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '../models/user.model';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  userForm=new User();
   constructor() { }
 
   ngOnInit() {
---
+++ studentcoursesapp/src/app/models/user.model.ts
@@ -0,0 +1,14 @@
+export class User {
+        // UserId:number;
+        // FirstName:string;
+        // LastName:string;
+        Username:string;
+        Password:string;
+        // IsActive:boolean;
+
+        constructor(Username='',Password='')
+        {
+            this.Username=Username;
+            this.Password=Password;
+        }
+}
